cbe0926ca530adfd1cb4d8d312fe73c06e176440
Dhruv Sorout <dhruvsorout0010@gmail.com>
2025-09-27 18:56:46 +0530

    add debouncing

--- week-3/Assg-1/script.js
+++ week-3/Assg-1/script.js
@@ -1,15 +1,28 @@
-// function calculateSum(){
-//     let a = parseFloat(document.getElementById("num1").value);
-//     let b = parseFloat(document.getElementById("num2").value);
+let timeout;
+function debounceCalculateSum(){
+    // delay the call to calculateSum until I've not been called for 100ms
+    // and I've been called once
+    // how do you cancel a clock
+    // clearTimeOut
+    clearTimeout(timeout);
+    timeout = setTimeout(()=>{
+        calculateSum()
+    }, 1000)
+}
 
-//     fetch("http://localhost:3001/sum?a=" + a + "&b=" + b)
-//         .then(function (response){
-//             response.text()
-//                 .then(function (ans){
-//                     document.getElementById("finalSum").innerHTML = "Sum of a and b is: " + ans;
-//                 })
-//         });
-// }
+
+function calculateSum(){
+    let a = parseFloat(document.getElementById("num1").value);
+    let b = parseFloat(document.getElementById("num2").value);
+
+    fetch("http://localhost:3001/sum?a=" + a + "&b=" + b)
+        .then(function (response){
+            response.text()
+                .then(function (ans){
+                    document.getElementById("finalSum").innerHTML = "Sum of a and b is: " + ans;
+                })
+        });
+}
 
 async function calculateSum2(){
     let a = parseFloat(document.getElementById("num1").value);
